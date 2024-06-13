065c9e193f78a4273fb7e61f256c3fb6e9b2ae35
Ruben Goncalves <ruben.goncalves@kuehne-nagel.com>
2024-06-13 11:13:17 +0100

    commit#7

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -3,7 +3,6 @@
 rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
-mkdir -p ./.amplify-hosting/static
 
 cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
