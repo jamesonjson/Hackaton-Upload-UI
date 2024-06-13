74388f0075c7fe3704b89c7d85b01c62a4eb38bd
Ruben Goncalves <ruben.goncalves@kuehne-nagel.com>
2024-06-13 10:02:08 +0100

    Third commit

---
+++ bin/postbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./.amplify-hosting
+
+mkdir -p ./.amplify-hosting/compute
+
+cp -r ./dist ./.amplify-hosting/compute/default
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+
+cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
